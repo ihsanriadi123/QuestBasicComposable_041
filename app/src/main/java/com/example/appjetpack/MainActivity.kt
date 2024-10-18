cd26e02c37cfad7c95f7ed5df92bd707a0e0e704
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-10-18 14:38:27 +0700

    menambahkan BasicCompose dengan padding menggunakan innerPadding di Scaffold

--- app/src/main/java/com/example/appjetpack/MainActivity.kt
+++ app/src/main/java/com/example/appjetpack/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : ComponentActivity() {
 
                         ihsan = Modifier.padding(innerPadding)
                     )
+        //
                 }
             }
         }
